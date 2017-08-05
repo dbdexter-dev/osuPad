1cec041696e5645d0432d7c84bb1eba1f12ff8aa
dbdexter-dev <dbdexter-dev@gmail.com>
2017-08-05 14:40:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pro X Mini keypad firmware
+
+Since I wanted to add buttons to my pad (just like the official osu! keypad), I rewrote the firmware that runs on it
---
+++ firmware.ino
@@ -0,0 +1,81 @@
+// Open source zzqProX keypad firmware
+#include <Keyboard.h>
+#define KEYNUM 5
+#define LEDNUM 2
+#define DEBOUNCE_DELAY 500
+
+const unsigned char keys[KEYNUM] = {'z', 'x', '`', KEY_F2, KEY_LEFT_SHIFT}; // Keys to emulate
+const int keyPins[KEYNUM] = {8, 15, 5, 6, 7};								// Pins corresponding to the keys above
+const int ledPins[LEDNUM] = {3, 10};										// Optional LED pins
+int oldKeyStatus[KEYNUM];
+
+void setup()
+{
+	int i;
+
+	#ifdef DEBUG
+	Serial.begin(9600);
+	while(!(Serial.available())
+		delay(10);
+	#endif
+
+	// Initialize LEDs
+	for(i=0; i<LEDNUM; i++)
+	{
+		pinMode(ledPins[i], OUTPUT);
+		digitalWrite(ledPins[i], 0);
+	}
+
+	// Initialize keys
+	for(i=0; i<KEYNUM; i++)
+	{
+		pinMode(keyPins[i], INPUT);
+		digitalWrite(keyPins[i], HIGH);
+	}
+
+	#ifdef DEBUG
+	Serial.println("Debug mode activated. No keyboard output will be generated.");
+	#else
+	Keyboard.begin();
+	#endif
+}
+
+void loop()
+{
+	int i;
+	int keyStatus;
+
+	for(i=0; i<KEYNUM; i++)
+	{
+		keyStatus = digitalRead(keyPins[i]);
+		if(keyStatus != oldKeyStatus[i])
+		{
+			delayMicroseconds(DEBOUNCE_DELAY);
+			if(keyStatus == digitalRead(keyPins[i]))
+			{
+				if(!keyStatus)
+				{
+					#ifdef DEBUG
+					Serial.println((int)keys[i]);
+					#else
+					Keyboard.press(keys[i]);
+					#endif
+					if(i < LEDNUM)
+						digitalWrite(ledPins[i], 1);
+				}
+				else
+				{
+					#ifdef DEBUG
+					Serial.println((int)keys[i]);
+					#else
+					Keyboard.release(keys[i]);
+					#endif
+					if(i < LEDNUM)
+						digitalWrite(ledPins[i], 0);
+				}
+				oldKeyStatus[i] = keyStatus;
+			}
+		}
+	}
+	delayMicroseconds(500);
+}
