4d7c1d65be3073462c7a92b55b6276bcd5bb0411
dbdexter-dev <dbdexter-dev@gmail.com>
2017-08-05 15:54:06 +0200

    Cleanup

--- firmware.ino
+++ firmware.ino
@@ -7,7 +7,6 @@
 const unsigned char keys[KEYNUM] = {'z', 'x', '`', KEY_F2, KEY_LEFT_SHIFT}; // Keys to emulate
 const int keyPins[KEYNUM] = {8, 15, 5, 6, 7};								// Pins corresponding to the keys above
 const int ledPins[LEDNUM] = {3, 10};										// Optional LED pins
-int oldKeyStatus[KEYNUM];
 
 void setup()
 {
@@ -15,8 +14,8 @@ void setup()
 
 	#ifdef DEBUG
 	Serial.begin(9600);
-	while(!(Serial.available())
-		delay(10);
+	#else
+	Keyboard.begin();
 	#endif
 
 	// Initialize LEDs
@@ -32,18 +31,13 @@ void setup()
 		pinMode(keyPins[i], INPUT);
 		digitalWrite(keyPins[i], HIGH);
 	}
-
-	#ifdef DEBUG
-	Serial.println("Debug mode activated. No keyboard output will be generated.");
-	#else
-	Keyboard.begin();
-	#endif
 }
 
 void loop()
 {
 	int i;
 	int keyStatus;
+	static int oldKeyStatus[KEYNUM];
 
 	for(i=0; i<KEYNUM; i++)
 	{
@@ -56,6 +50,7 @@ void loop()
 				if(!keyStatus)
 				{
 					#ifdef DEBUG
+					Serial.print("+");
 					Serial.println((int)keys[i]);
 					#else
 					Keyboard.press(keys[i]);
@@ -66,6 +61,7 @@ void loop()
 				else
 				{
 					#ifdef DEBUG
+					Serial.print("-");
 					Serial.println((int)keys[i]);
 					#else
 					Keyboard.release(keys[i]);
