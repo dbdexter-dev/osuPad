44d4b4df74be914d040bb91c75eab398fc9d75a9
dbdexter-dev <dbdexter-dev@gmail.com>
2017-08-06 10:55:52 +0200

    More efficient debouncing

--- firmware.ino
+++ firmware.ino
@@ -4,9 +4,10 @@
 #define LEDNUM 2
 #define DEBOUNCE_DELAY 500
 
-const unsigned char keys[KEYNUM] = {'z', 'x', '`', KEY_F2, KEY_LEFT_SHIFT}; // Keys to emulate
+const unsigned char keys[KEYNUM] = {'z', 'x', KEY_F1, KEY_F2, KEY_LEFT_SHIFT}; // Keys to emulate
 const int keyPins[KEYNUM] = {8, 15, 5, 6, 7};								// Pins corresponding to the keys above
 const int ledPins[LEDNUM] = {3, 10};										// Optional LED pins
+int oldKeyStatus[KEYNUM];
 
 void setup()
 {
@@ -14,8 +15,8 @@ void setup()
 
 	#ifdef DEBUG
 	Serial.begin(9600);
-	#else
-	Keyboard.begin();
+	while(!(Serial.available())
+		delay(10);
 	#endif
 
 	// Initialize LEDs
@@ -31,47 +32,51 @@ void setup()
 		pinMode(keyPins[i], INPUT);
 		digitalWrite(keyPins[i], HIGH);
 	}
+
+	#ifdef DEBUG
+	Serial.println("Debug mode activated. No keyboard output will be generated.");
+	#else
+	Keyboard.begin();
+	#endif
 }
 
 void loop()
 {
 	int i;
 	int keyStatus;
-	static int oldKeyStatus[KEYNUM];
 
 	for(i=0; i<KEYNUM; i++)
 	{
 		keyStatus = digitalRead(keyPins[i]);
 		if(keyStatus != oldKeyStatus[i])
 		{
-			delayMicroseconds(DEBOUNCE_DELAY);
-			if(keyStatus == digitalRead(keyPins[i]))
+			if(!keyStatus)
 			{
-				if(!keyStatus)
-				{
-					#ifdef DEBUG
-					Serial.print("+");
-					Serial.println((int)keys[i]);
-					#else
-					Keyboard.press(keys[i]);
-					#endif
-					if(i < LEDNUM)
-						digitalWrite(ledPins[i], 1);
-				}
-				else
-				{
-					#ifdef DEBUG
-					Serial.print("-");
-					Serial.println((int)keys[i]);
-					#else
-					Keyboard.release(keys[i]);
-					#endif
-					if(i < LEDNUM)
-						digitalWrite(ledPins[i], 0);
-				}
-				oldKeyStatus[i] = keyStatus;
+				#ifdef DEBUG
+				Serial.println((int)keys[i]);
+				#else
+				Keyboard.press(keys[i]);
+				#endif
+				if(i < LEDNUM)
+					digitalWrite(ledPins[i], 1);
 			}
+			else
+			{
+				#ifdef DEBUG
+				Serial.println((int)keys[i]);
+				#else
+				Keyboard.release(keys[i]);
+				#endif
+				if(i < LEDNUM)
+					digitalWrite(ledPins[i], 0);
+			}
+			oldKeyStatus[i] = keyStatus;
 		}
 	}
-	delayMicroseconds(500);
+	/**
+	* Debouncing is done by sampling after waiting a bit, so basically
+	* the first edge detected makes the key status flip. Subsequent
+	* transitions due to bouncing are simply not sampled
+	*/
+	delayMicroseconds(DEBOUNCE_DELAY);
 }
